9e602ed52634751b15b5319a4dfdb0db7aeb35d9
Alvaro Flores <alvaro.flores01@gmail.com>
2017-02-28 23:18:15 -0800

    adds day mode and night mode fuctionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-function daymode() {
-
-}
 function nightmode() {
-  getElementsByClassName("container")
-  
+  document.body.style.background = "#142634";
+  document.body.style.color = "white";
+  document.getElementsByTagName('h3').style.color = "white";
+}
+function daymode() {
+  document.body.style.background = "white";
+  document.body.style.color = "#142634";
+  document.getElementsByTagName('h3').style.color = "#142634";
 }
